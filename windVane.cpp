b66e1416848205602990c47fd8cfc400a4558e14
Luciano Costa <lucianomc@ic.ufal.br>
2017-02-07 23:18:33 -0300

    Windvane with rotation

--- windVane.cpp
+++ windVane.cpp
@@ -5,21 +5,22 @@ using namespace std;
 
 GLsizei w = 400, h = 400;
 
+GLfloat c1 = 0.03,
+        c2 = 0.05,
+        c3 = 0.07;
+
+//parametros de redering
 void init(void)
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);    //Paints the background
-    glClear(GL_COLOR_BUFFER_BIT);           //Enable background color
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(0.0, 200.0, 0.0, 150.0, -1.0, 1.0);
+    glClearColor(1.0f, 0.5f, 0.0f, 1.0f);    //Paints the background
 }
 
+// call to draw
 void displayMe(void)
 {
-    //glClear(GL_COLOR_BUFFER_BIT);
-
-    //glMatrixMode(GL_PROJECTION);
-
-    glLoadIdentity();  
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT); 
 
     glBegin(GL_LINES);
         glColor3f(1.0f, 0.0f, 0.0f);
@@ -28,28 +29,28 @@ void displayMe(void)
     glEnd();
 
     glBegin(GL_POLYGON);
-        glColor3f(0.0f, 0.0f, 1.0f);
+        glColor3f(0.0f + c1, 0.0f, 0.0f);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(0.5, -0.2, 0.0);
         glVertex3f(0.5, 0.2, 0.0);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glColor3f(0.0f, 1.0f, 0.0f);
+        glColor3f(0.1f + c1, 0.0f, 0.0f);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(0.2, 0.5, 0.0);
         glVertex3f(-0.2, 0.5, 0.0);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glColor3f(1.0f, 1.0f, 0.0f);
+        glColor3f(0.2f + c1, 0.0f, 0.0f);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(-0.5, 0.2, 0.0);
         glVertex3f(-0.5, -0.2, 0.0);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glColor3f(1.0f, 0.5f, 1.0f);
+        glColor3f(0.3f + c1, 0.0f, 0.0f);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(0.2, -0.5, 0.0);
         glVertex3f(-0.2, -0.5, 0.0);
@@ -60,10 +61,9 @@ void displayMe(void)
 
 //Initiate parameters to rendering
 
-
 void changeWindowSize( GLsizei w, GLsizei h){
     if (h == 0) h = 1;
-
+    //define a área dentro da janela
     glViewport( 0, 0, w, h);
 
     //glMatrixMode(GL_PROJECTION);
@@ -75,14 +75,34 @@ void changeWindowSize( GLsizei w, GLsizei h){
         gluOrtho2D(.0f, 250.0f*w/h, .0f, 250.0f);
 }
 
+void mouse (int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON){
+        c1 += 0.05;
+        c2 += 0.05;
+        c3 += 0.05;
+        //cout << "entrou";
+    }
+    else
+        c1 -= 0.05;
+        c2 -= 0.05;
+        c3 -= 0.05;
+    glutPostRedisplay();
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+    //Que tipo de exibição deve ser usado quando a janela é criada
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);   //Display mode
+    //posição da janela
     glutInitWindowPosition(350, 150);
     //glutInitWindowSize(300, 300);
+    //tamanho em pixels
     glutInitWindowSize(w, h);
+    //Cria a janela
     glutCreateWindow("Flor de Abril");  //Create a window
+    glutMouseFunc(mouse);
 
     init();
 
